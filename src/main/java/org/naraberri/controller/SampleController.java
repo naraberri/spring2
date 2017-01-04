e4196292dcc04a2631da73aad7ba9b3db7adc497
LEE SEUNG CHAN <LEE SEUNG CHAN@DESKTOP-SBKPE9B>
2017-01-04 20:22:17 +0900

    update

---
+++ src/main/java/org/naraberri/controller/SampleController.java
@@ -0,0 +1,14 @@
+package org.naraberri.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/sample")
+public class SampleController {
+
+	@RequestMapping("/hello")
+	public String sayHello() {
+		return "Hello World";
+	}
+}
